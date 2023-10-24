93dbd0302a51da34dba874443d7bc88971294111
FabianoPires1234 <148865835+FabianoPires1234@users.noreply.github.com>
2023-10-24 08:02:51 -0300

    Add files via upload

---
+++ isso.js
@@ -0,0 +1,25 @@
+function carConteudo(){
+
+
+    document.getElementById("conteudo").innerHTML = "<p> tete2</p>";
+   
+}
+
+
+
+function carinteracao(){
+
+
+    document.getElementById("interacao").innerHTML = "<p> tete3</p>";
+   
+}
+
+
+
+
+function carmelhor(){
+
+
+    document.getElementById("melhor").innerHTML = "<p> tete1</p>";
+   
+}
